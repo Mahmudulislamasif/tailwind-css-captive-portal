3e6187d9f0c49160f447786f782edb0c649f5bc1
ASIF\mahmu <Mohammad Mahmudul Islam>
2023-05-29 17:01:03 +0600

    add

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -1,12 +1,23 @@
 
-  var countDown=new Date("May 30, 2023 00:00:00").getTime();
-  var x=setInterval(function(){
-    var now=new Date().getTime();
-    var distance=countDown-now;
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    document.getElementById("hours").innerHTML=hours;
-    document.getElementById("minutes").innerHTML=minutes;
-    document.getElementById("seconds").innerHTML=seconds;
-  },1000)
+var countDown = new Date("May 30, 2023 00:00:00").getTime();
+var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDown - now;
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Add leading zeros if needed
+  hours = addLeadingZero(hours);
+  minutes = addLeadingZero(minutes);
+  seconds = addLeadingZero(seconds);
+
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+}, 1000);
+
+function addLeadingZero(value) {
+  return value < 10 ? "0" + value : value;
+}
+
